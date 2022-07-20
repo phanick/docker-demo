d0b800a0b56fb4041a53314351de290766af6c78
phanick <39258517+phanick@users.noreply.github.com>
2022-07-20 22:31:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, this is Kubernetes demo on 18July 1021"
+    return "Hello, this is Kubernetes demo on 20July 1031pm"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
